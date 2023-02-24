39844316dfaaa5f4eab6d0274918c1c84fb3d415
Ian Laidlaw <ian@kaboodles.com>
2023-02-23 20:08:15 -0500

    Updated error handling and inventory initialization

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -10,13 +8,25 @@ public class Main {
         System.out.print("Enter a password for the vending machine: ");
 
         String password = scanner.nextLine();
-        System.out.printf("Your password will be: %s", password);
+        System.out.printf("Your password will be: %s\n", password);
 
         VendingMachine vendingMachine = new VendingMachine(password);
 
+        ItemInfo[] items = {new ItemInfo("Mountain Dew", 1.75),
+                new ItemInfo("Diet Mountain Dew", 1.75),
+                new ItemInfo("Dr. Pepper Zero", 1.75),
+                new ItemInfo("Lemonade", 1.75), new ItemInfo("Pepsi", 1.75),
+                new ItemInfo("Dr. Pepper", 1.75), new ItemInfo("Water", 2.25),
+                new ItemInfo("Energy Drink", 3.00)};
+
+        vendingMachine.login(password);
+
+        for (ItemInfo item : items) {
+            vendingMachine.addItem(item);
+        }
 
         System.out.println(vendingMachine.showInventory());
-
+        showHelp();
 
         String input;
         while (run) {
@@ -32,53 +42,46 @@ public class Main {
 
             if (splitInput.length == 1) {
                 switch (lowercaseInput[0]) {
-                    case "h", "help" -> {
-                        showHelp();
-                    }
-                    case "logoff", "exit" -> {
-                        vendingMachine.logoff();
-                    }
+                    case "h", "help" -> showHelp();
+                    case "logoff", "exit" -> vendingMachine.logoff();
                 }
             } else {
                 lowercaseInput[1] = splitInput[1].toLowerCase();
 
                 switch (lowercaseInput[0]) {
                     case "i", "insert" -> {
-                        Coin coin = Coin.valueOf(lowercaseInput[1]);
-                        vendingMachine.insertCoin(coin);
+                        try {
+                            Coin coin = Coin.fromString(lowercaseInput[1]);
+                            vendingMachine.insertCoin(coin);
+                        } catch (IllegalArgumentException e) {
+                            System.out.printf("%s is not a valid coin\n",
+                                    lowercaseInput[1]);
+                        }
                     }
                     case "b", "buy" -> {
-                        if (!vendingMachine.isValidSelection(
-                                Integer.parseInt(lowercaseInput[1]))) {
-                            System.out.println("Invalid selecetion input");
-                        } else {
-                            vendingMachine.buy(lowercaseInput[1]);
+                        try {
+                            if (!vendingMachine.isValidSelection(
+                                    Integer.parseInt(lowercaseInput[1]))) {
+                                System.out.println("Invalid product number");
+                            } else {
+                                vendingMachine.buy(lowercaseInput[1]);
+                            }
+                        } catch (NumberFormatException e) {
+                            System.out.printf(
+                                    "%s is not in the range of acceptable " +
+                                            "inputs\n", lowercaseInput[1]);
                         }
                     }
                     case "s", "show" -> {
                         switch (lowercaseInput[1]) {
-                            case "i", "inv", "inventory" -> {
-                                System.out.println(
-                                        vendingMachine.showInventory());
-                            }
-                            case "h", "help" -> {
-                                showHelp();
-                            }
+                            case "i", "inv", "inventory" -> System.out.println(
+                                    vendingMachine.showInventory());
+                            case "h", "help" -> showHelp();
                         }
                     }
-                    case "login" -> {
-                        if (!vendingMachine.checkPassword(splitInput[1])) {
-                            System.out.println("Incorrect password");
-                        } else {
-                            vendingMachine.login();
-                        }
-                    }
-                    case "exit", "logoff" -> {
-                        vendingMachine.logoff();
-                    }
-                    case "q", "quit" -> {
-                        run = false;
-                    }
+                    case "login" -> vendingMachine.login(splitInput[1]);
+                    case "exit", "logoff" -> vendingMachine.logoff();
+                    case "q", "quit" -> run = false;
 
                 }
             }
@@ -89,9 +92,6 @@ public class Main {
     private static boolean isValidInput(String input) {
         String[] inputs = input.split(" ");
 
-        ArrayList<String> validCoins = new ArrayList<>(
-                Arrays.asList("penny", "p", "nickel", "n", "dime", "d",
-                        "quarter", "q"));
 
         if (inputs.length == 1) {
             switch (inputs[0]) {
@@ -106,10 +106,7 @@ public class Main {
             }
         } else {
             switch (inputs[0].toLowerCase()) {
-                case "i", "insert" -> {
-                    return validCoins.contains(inputs[1].toLowerCase());
-                }
-                case "b", "buy", "q", "quit", "l", "login" -> {
+                case "i", "insert", "b", "buy", "q", "quit", "l", "login" -> {
                     return true;
                 }
                 case "s", "show" -> {
--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -5,12 +5,14 @@ public class VendingMachine {
     private VendingMachineState state;
     private double inputCoinAmount;
 
+    private int numItems;
 
     public VendingMachine(String password) {
         inventory = new Inventory();
         coins = new CoinPool();
         this.password = password;
         inputCoinAmount = 0;
+        numItems = 0;
     }
 
     public String showInventory() {
@@ -19,8 +21,14 @@ public class VendingMachine {
 
         for (ItemInfo info : inventory) {
             output.append(
-                    String.format("%-10s %-4.2f", info.getItem().name(),
+                    String.format("[%d] %-20s $%-4.2f  ",
+                            inventory.getID(info.getItem().name()),
+                            info.getItem().name(),
                             info.getPrice()));
+            if (i % 4 == 0) {
+                output.append("\n");
+            }
+            i++;
         }
 
         return output.toString();
@@ -28,6 +36,12 @@ public class VendingMachine {
 
     public void buy(String itemID) {
         double itemPrice = inventory.getPrice(itemID);
+
+        if (inputCoinAmount < itemPrice) {
+            System.out.printf("Unable to buy %s. Please insert more coins\n",
+                    inventory.getName(itemID));
+            return;
+        }
 
         if (!hasChange(inputCoinAmount - itemPrice)) {
             System.out.printf("Unable to buy %s. Not enough coins for change\n",
@@ -43,12 +57,16 @@ public class VendingMachine {
         }
     }
 
-    public boolean checkPassword(String input) {
-        return input.equals(password);
+    public boolean checkPassword(String password) {
+        return password.equals(this.password);
     }
 
-    public void login() {
-        state = VendingMachineState.OPERATOR;
+    public void login(String password) {
+        if (checkPassword(password)) {
+            state = VendingMachineState.OPERATOR;
+        } else {
+            System.out.println("Incorrect password. Please try again.");
+        }
     }
 
     public void logoff() {
@@ -58,6 +76,18 @@ public class VendingMachine {
     public void insertCoin(Coin coin) {
         inputCoinAmount += coin.getValue();
         coins.add(coin);
+        System.out.printf("Inserted %s\n", coin.getName());
+    }
+
+    public void addItem(ItemInfo item) {
+        if (state != VendingMachineState.OPERATOR) {
+            System.out.println(
+                    "You must be in Operator mode to add items to the vending" +
+                            " machine");
+            return;
+        }
+
+        inventory.addItem(item, ++numItems);
     }
 
     public boolean isValidSelection(int index) {
@@ -68,8 +98,8 @@ public class VendingMachine {
         CoinAmounts change = calculateChange(inputCoinAmount);
 
         System.out.printf(
-                "Your change is: %.2f quarters, %.2f dimes, %.2f nickels, %" +
-                        ".2f pennies", change.quarters, change.dimes,
+                "Your change is: %d quarters, %d dimes, %d nickels, %" +
+                        "d pennies\n", change.quarters, change.dimes,
                 change.nickels, change.pennies);
     }
 
@@ -80,13 +110,13 @@ public class VendingMachine {
         int numPennies = 0;
 
         while (amount > 0) {
-            if (amount > 0.25) {
+            if (amount >= 0.25) {
                 numQuarters++;
                 amount -= 0.25;
-            } else if (amount > 0.10) {
+            } else if (amount >= 0.10) {
                 numDimes++;
                 amount -= 0.10;
-            } else if (amount > 0.05) {
+            } else if (amount >= 0.05) {
                 numNickels++;
                 amount -= 0.05;
             } else {
@@ -108,8 +138,8 @@ public class VendingMachine {
     }
 
 
-    private record CoinAmounts(double quarters, double dimes, double nickels,
-                               double pennies) {
+    private record CoinAmounts(int quarters, int dimes, int nickels,
+                               int pennies) {
 
     }
 }
